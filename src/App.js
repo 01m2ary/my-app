2a8202175b1e1a93866462ec93a3bd1d1ef19a39
01m2ary <01m2ary@gmail.com>
2024-07-31 00:06:38 +0300

    commint on" login and Home and Resiption and listpatients and addinvoice"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+/*
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Mlogin from './Mlogin/Mlogin';
+import MHome from './MHome/MHome';
+import MReceptionist from './MReceptionist/MReceptionist';
+import MListPatients from './MListPatients/MListPatients';
+import MAddInvoice from './MAddInvoice/MAddInvoice'; // Import the MAddInvoice component
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mlogin />} />
+        <Route path="/home" element={<MHome />} />
+        <Route path="/receptionist" element={<MReceptionist />} />
+        <Route path="/list-patients" element={<MListPatients />} />
+        <Route path="/add-invoice" element={<MAddInvoice />} /> 
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+*/
+
+
+// App.js
+/*
+import React from 'react';
+import Mlogin from './Mlogin/Mlogin'; // تأكد من مسار الملف الصحيح
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mlogin />
     </div>
   );
 }
 
 export default App;
+*/
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Mlogin from './Mlogin/Mlogin';
+import MHome from './MHome/MHome';
+import MReceptionist from './MReceptionist/MReceptionist';
+import MListPatients from './MListPatients/MListPatients';
+import MAddInvoice from './MAddInvoice/MAddInvoice'; // استيراد مكون MAddInvoice
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mlogin />} />
+        <Route path="/home" element={<MHome />} />
+        <Route path="/receptionist" element={<MReceptionist />} />
+        <Route path="/list-patients" element={<MListPatients />} />
+        <Route path="/add-invoice" element={<MAddInvoice />} /> {/* إضافة المسار لواجهة MAddInvoice */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
